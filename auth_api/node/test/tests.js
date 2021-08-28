a275e31d1c324566a6aa3d36b94bd33f16728dd6
Carlos Guerrero <reach.117.war@gmail.com>
2021-08-28 16:27:10 -0500

    feat: Add login tests

--- auth_api/node/test/tests.js
+++ auth_api/node/test/tests.js
@@ -1,13 +1,33 @@
 import chai from 'chai';
 import { loginFunction } from '../services/login'
 import { protectFunction } from '../services/protected'
-
+import connection from '../db/connection';
 const expect = chai.expect;
 
 describe('loginFunction()', function () {
-  it('Test login', function () {
-
-    expect("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI").to.be.equal(loginFunction("admin", "secret"));
+  before(() =>  {
+    require('dotenv').config();
+    connection.init();
+  });
+  it('Test login', function (done) {
+    loginFunction("admin", "secret").then(x => {
+      expect(x).to.be.equal("eyJhbGciOiJIUzI1NiJ9.YWRtaW4.jxjAJS_99b0l_21irrGWUOw_6RQsYqy1cT3DoIHil44");
+      done();
+    })
+  });
+  it('Invalid Request', function (done) {
+    loginFunction("", "")
+    .catch(err => {
+      expect("Invalid credentials").to.be.equal(err.message);
+      done();
+    })
+  });
+  it('Invalid Credentials', function (done) {
+    loginFunction("admin", "s")
+    .catch(err => {
+      expect("Invalid credentials").to.be.equal(err.message);
+      done();
+    })
   });
 });
 
